13f0c57aaad6d6f9bb78b34d757ddbe5a678804f
FerielBk <132261060+FerielBk@users.noreply.github.com>
2024-09-15 09:27:05 +0200

    Added easing function
    implemented easeOoutCubic for smooth transitions
    updated the home() function 
    added setServoPosition function to apply easing function for setting the 18 servo's positions

--- Mouvement/Home_Pos.ino
+++ Mouvement/Home_Pos.ino
@@ -1,5 +1,6 @@
 #include <Wire.h>
 #include <Adafruit_PCA9685.h>
+#include <math.h>
 
 // Create PCA9685 objects for left and right sides
 Adafruit_PCA9685 pwmLeft = Adafruit_PCA9685();
@@ -30,14 +31,17 @@ uint16_t servoAngleToPWM(int angle)
 {
   return map(angle, 0, 200, SERVOMIN, SERVOMAX);
 }
+float easeOutCubic(float x){
+    return 1- pow(1 - x, 3);
+}
 
 // Function to set the servos to the home position
 void home() 
 {
   // Set left servos to home position
-  pwmLeft.setPWM(0, 0, servoAngleToPWM(standingPositionsLeft[0])); // Left front leg - Servo 1
+  /* pwmLeft.setPWM(0, 0, servoAngleToPWM(standingPositionsLeft[0])); // Left front leg - Servo 1
   pwmLeft.setPWM(1, 0, servoAngleToPWM(standingPositionsLeft[1])); // Left front leg - Servo 2
-  pwmLeft.setPWM(2, 0, servoAngleToPWM(standingPositionsLeft[2])); // Left front leg - Servo 3
+  pwmLeft.setPWM(2, 0, servoAngleToPWM(standingPositionsLeft[2])); // Left front leg - Servo 3 --
   
   pwmLeft.setPWM(3, 0, servoAngleToPWM(standingPositionsLeft[3])); // Left middle leg - Servo 1
   pwmLeft.setPWM(4, 0, servoAngleToPWM(standingPositionsLeft[4])); // Left middle leg - Servo 2
@@ -58,9 +62,36 @@ void home()
   
   pwmRight.setPWM(6, 0, servoAngleToPWM(standingPositionsRight[6])); // Right rear leg - Servo 1
   pwmRight.setPWM(7, 0, servoAngleToPWM(standingPositionsRight[7])); // Right rear leg - Servo 2
-  pwmRight.setPWM(8, 0, servoAngleToPWM(standingPositionsRight[8])); // Right rear leg - Servo 3
+  pwmRight.setPWM(8, 0, servoAngleToPWM(standingPositionsRight[8])); // Right rear leg - Servo 3*/
+  for(int i=0; i<NUM_SERVOS_PER_SIDE; i++){
+    pwmLeft.setPWM(i,0, servoAngleoPWM(standingPositionLeft[i]));
+     pwmRight.setPWM(i,0, servoAngleoPWM(standingPositionRight[i]));
 
 }
+
+
+void setServoPosition(float t)
+{ 
+float easeValue=easeOutCubic(t);
+for (int i=0;i<NUM_SERVOS_PER_SIDE;i++){
+  
+/*int angleLeft=standingPositionLeft[i]+(int)((200-standingPositionLeft[i])*easeValue);
+int angleRight=standingPositionRight[i]+(int)((200-standingPositionRight[i])*easeValue);
+pwmLeft.setPWM(i,0, servoAngleToPWM(angleLeft);
+pwmRight.setPWM(i,0, servoAngleToPWM(angleRight);*/
+  
+  //apply easing function
+  int pwmPosition= map(easeValue, 0, 1, SERVOMIN, SERVOMAX); 
+  for(int i=0;i<NUM_SERVOS_PER_SIDE;i++){
+    pwmLeft.setPWM(i,0, pwmPosition);
+  }
+   for(int i=0;i<NUM_SERVOS_PER_SIDE;i++){
+    pwmRight.setPWM(i,0, pwmPosition);
+  }
+    
+}
+
+
 
 void setup() 
 {
@@ -70,11 +101,23 @@ void setup()
 
   pwmRight.begin();
   pwmRight.setPWMFreq(60); // Set frequency to 60 Hz for right servos
-
+ home();
 }
 
 void loop() 
 {
-   // Call the home function to set initial positions
-  home();
+  //easing motion from 0 to 1
+  // 0 for the starting transition and 1 for the end 
+   for(float t=0;t<=1;t+=0.01)
+     {
+       setSeroPosition(t);
+       delay();
+     }
+  //from 1 to 0 
+   for(float t=1;t>=0;t-=0.01)
+     {
+       setSeroPosition(t);
+       delay();
+     }
+  
 }
