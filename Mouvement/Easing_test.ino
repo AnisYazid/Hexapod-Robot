f063d0d48d33b2aed82c83fd83dca729d108997e
AnisYazid <anisyazid02@gmail.com>
2024-09-15 11:41:15 +0100

    correct some syntax errors

--- Mouvement/Easing_test.ino
+++ Mouvement/Easing_test.ino
@@ -10,15 +10,18 @@ const int SERVOMax=2100 ;
 
 int standingPosition[NUM_SERVOS] ={ 100, 100 , 100};
 
-uint16_t servoAngleToPWM( int angle){
+uint16_t servoAngleToPWM( int angle)
+{
   return map(angle,0,180, SERVOMIN, SERVOMAX);
 }
 
-float easeOutCubic(float x){
+float easeOutCubic(float x)
+{
   return pow(x-1,3)+1;
 }
 
-void home(){
+void home()
+{
   for (int i=0;i<NUM_SERVOS;i++){
 
 pwm.setPWM(i,0,servoAngleToPWM(standingPosition[i]));
@@ -26,7 +29,8 @@ pwm.setPWM(i,0,servoAngleToPWM(standingPosition[i]));
 }
 
 
-void setServoPosition(float t){
+void setServoPosition(float t)
+{
  float easeValue= easeOutCubic(t);
 for(int i=0;i<NUM8SERVOS;i++){
 int position =ma( easeValue,0,1, SERVOMIN,SERVOMAX);
@@ -34,22 +38,27 @@ pwm.setPWM(i,0,position);
 }
 }
 
-void setup { 
+void setup ()
+{ 
   pwm.begin();
 pwm.setPWMFreq(60);
 home();
 }
 
-void loop {
-  for(float t=0;t<=1; t+=0.01){
-setServoPosition(t);
-delay();
+void loop ()
+{
+  for(float t=0;t<=1; t+=0.01)
+  {
+    setServoPosition(t);
+    delay();
   }
 delay();
-for(float t=1;t>=0; t-=0.01){
-setServoPosition(t);
-delay();
+for(float t=1;t>=0; t-=0.01)
+  {
+    setServoPosition(t);
+    delay();
   }
+}
 
 
 
